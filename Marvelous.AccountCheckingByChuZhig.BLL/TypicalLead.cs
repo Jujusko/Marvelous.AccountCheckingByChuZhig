48997805c6f1cf27a53aeed98e8dcadf988cb1c2
Dinar <doouw@mail.ru>
2022-03-16 19:09:42 +0500

    Try to take response from API

---
+++ Marvelous.AccountCheckingByChuZhig.BLL/TypicalLead.cs
@@ -0,0 +1,22 @@
+﻿using Marvelous.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marvelous.AccountCheckingByChuZhig.BLL
+{
+    public class TypicalLead
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public List<AccountResponse> Accounts { get; set; }
+        public Role Role { get; set; }
+        public bool IsBanned { get; set; }
+    }
+}
