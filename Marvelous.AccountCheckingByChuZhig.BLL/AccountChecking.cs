186bf5f6394075f68b2dc308f187a00affa29489
Dinar <doouw@mail.ru>
2022-03-16 19:45:24 +0500

    Added method to take all leads

---
+++ Marvelous.AccountCheckingByChuZhig.BLL/AccountChecking.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Text.Json;
+
+namespace Marvelous.AccountCheckingByChuZhig.BLL
+{
+    public class AccountChecking
+    {
+        private readonly string _urlToTakeAllLeads = "https://piter-education.ru:6010/api/leads";
+
+        public List<LeadResponse> GetAllLeads()
+        {
+            WebRequest myWebRequest = WebRequest.Create($"{_urlToTakeAllLeads}");
+            WebResponse myWebResponse = myWebRequest.GetResponse();
+
+            string text;
+            using (var sr = new StreamReader(myWebResponse.GetResponseStream()))
+            {
+                text = sr.ReadToEnd();
+            }
+            List<LeadResponse> result = JsonConvert.DeserializeObject<List<LeadResponse>>(text);
+            return result;
+        }
+    }
+}
--- Marvelous.AccountCheckingByChuZhig.BLL/LeadResponse.cs
+++ Marvelous.AccountCheckingByChuZhig.BLL/LeadResponse.cs
@@ -3,12 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Marvelous.AccountCheckingByChuZhig.BLL
 {
-    public class TypicalLead
+    public class LeadResponse
     {
+        [JsonInclude]
         public int Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
