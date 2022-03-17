f0b716ed496d67b663dfd21997046c3dd1c38bbc
Aufniy Tiger <golenkotoxa@gmail.com>
2022-03-17 12:17:01 +0300

    added url methods

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Marvelous.AccountCheckingByChuZhig.BLL.Helpers;
 using Marvelous.AccountCheckingByChuZhig.BLL.Services;
 
 Console.WriteLine("Hello, World!");
 AccountChecking ac = new();
 var asc = ac.GetAllLeads();
 int a = 0;
+
+ var str = ReportURLs.GetLeadTransactionForPeriod(3, DateTime.Now, DateTime.Now.AddDays(4));
